2b2a4448e401bbd00a464014b6e1a334dcb1d35e
Joaquin-B <joaquinbecerra02@gmail.com>
2026-07-16 21:05:47 -0500

    Agregamos Vanguard y terminamos Login

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -12,21 +12,22 @@ import { Configuracion } from './modulos/configuracion/configuracion';
 import { Inventario } from './modulos/inventario/inventario';
 import { Compras } from './modulos/compras/compras';
 import { NoEncontro } from './modulos/no-encontro/no-encontro';
+import { validaruserGuard } from './guard/validaruser-guard';
 
 export const routes: Routes = [
     {
         path: '', component: Main, 
         children: [
-            { path: 'dashboard', component: Dashboard },
-            { path: 'productos', component: Productos },
-            { path: 'clientes', component: Clientes },
-            { path: 'usuarios', component: Usuarios },
-            { path: 'proveedores', component: Proveedores },
-            { path: 'reportes', component: Reportes },
-            { path: 'ventas', component: Ventas },
-            { path: 'configuracion', component: Configuracion },
-            { path: 'inventario', component: Inventario },
-            { path: 'compras', component: Compras },
+            { path: 'dashboard', component: Dashboard, canActivate:[validaruserGuard] },
+            { path: 'productos', component: Productos, canActivate:[validaruserGuard] },
+            { path: 'clientes', component: Clientes, canActivate:[validaruserGuard] },
+            { path: 'usuarios', component: Usuarios, canActivate:[validaruserGuard] },
+            { path: 'proveedores', component: Proveedores, canActivate:[validaruserGuard] },
+            { path: 'reportes', component: Reportes, canActivate:[validaruserGuard] },
+            { path: 'ventas', component: Ventas, canActivate:[validaruserGuard] },
+            { path: 'configuracion', component: Configuracion, canActivate:[validaruserGuard] },
+            { path: 'inventario', component: Inventario, canActivate:[validaruserGuard] },
+            { path: 'compras', component: Compras, canActivate:[validaruserGuard] },
             { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
         ]
     },
